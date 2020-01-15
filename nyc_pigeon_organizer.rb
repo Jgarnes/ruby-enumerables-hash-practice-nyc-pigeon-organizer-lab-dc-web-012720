9867e2ae5270b65473cd93ded820c20299161c37
Jeremy Garnes <j06garnes@gmail.com>
2020-01-15 19:38:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,48 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ new_hash = {} 
+  data[:gender].each do |key, value|
+   value.each do |name|
+   
+   new_hash[name] = {
+       :color => [],
+       :gender => [],
+       :lives => []
+     }
+  end 
+ end 
+ array_of_names = new_hash.keys
+    array_of_names.each do |name|
+ 
+      data[:color].each do |key, value|
+    
+    if value.include?(name)  
+      new_hash[name][:color] << key.to_s
+      end  
+     end 
+    end   
+    
+  array_of_names = new_hash.keys
+    array_of_names.each do |name|
+ 
+      data[:gender].each do |key, value|
+    
+    if value.include?(name)  
+      new_hash[name][:gender] << key.to_s
+      end  
+     end 
+    end     
+    
+    array_of_names = new_hash.keys
+    array_of_names.each do |name|
+ 
+      data[:lives].each do |key, value|
+    
+    if value.include?(name)  
+      new_hash[name][:lives] << key.to_s
+      end  
+     end 
+    end  
+    new_hash
 end
