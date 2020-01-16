87d58165e41dfc6072a9bf727761af06fcce6d43
Jeremy Garnes <j06garnes@gmail.com>
2020-01-16 04:06:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,6 +10,7 @@ def nyc_pigeon_organizer(data)
        :gender => [],
        :lives => []
      }
+<<<<<<< HEAD
   end 
  end 
  array_of_names = new_hash.keys
@@ -46,3 +47,12 @@ def nyc_pigeon_organizer(data)
     end  
     new_hash
 end
+=======
+   end 
+   date[:color][color].includes(name) 
+   binding.pry
+   #? new_hash[name][:color] : nil 
+   
+   end 
+  end 
+>>>>>>> 95604f70e2b093f58e60d42fc4dfdc8e0b478cbe
